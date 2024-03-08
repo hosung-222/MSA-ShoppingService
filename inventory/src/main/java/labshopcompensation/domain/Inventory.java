1ab16a16786cf1989ef85261884a7ba49538a978
HoSung Lee 이호성 <gik11kr@naver.com>
2024-03-08 08:30:42 +0000

    재고 복구 로직

--- inventory/src/main/java/labshopcompensation/domain/Inventory.java
+++ inventory/src/main/java/labshopcompensation/domain/Inventory.java
@@ -55,24 +55,9 @@ public class Inventory {
     //>>> Clean Arch / Port Method
     //<<< Clean Arch / Port Method
     public static void increaseStock(OrderCancelled orderCancelled) {
-        //implement business logic here:
-
-        /** Example 1:  new item 
-        Inventory inventory = new Inventory();
-        repository().save(inventory);
-
-        */
-
-        /** Example 2:  finding and process
-        
-        repository().findById(orderCancelled.get???()).ifPresent(inventory->{
-            
-            inventory // do something
-            repository().save(inventory);
-
-
-         });
-        */
+        repository().findById(Long.valueOf(orderCancelled.getProductId())).ifPresent(inventory ->{
+			inventory.setStock(inventory.getStock() + orderCancelled.getQty());
+		    repository().save(inventory);
 
     }
     //>>> Clean Arch / Port Method
