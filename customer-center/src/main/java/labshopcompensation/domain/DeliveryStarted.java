1665a0824d922df625405aa78199c20e192f219a
HoSung Lee 이호성 <62132755+hosung-222@users.noreply.github.com>
2024-03-12 13:27:38 +0900

    commit

--- customer-center/src/main/java/labshopcompensation/domain/DeliveryStarted.java
+++ customer-center/src/main/java/labshopcompensation/domain/DeliveryStarted.java
@@ -2,13 +2,10 @@ package labshopcompensation.domain;
 
 import java.time.LocalDate;
 import java.util.*;
-import labshopcompensation.domain.*;
 import labshopcompensation.infra.AbstractEvent;
-import lombok.*;
+import lombok.Data;
 
-//<<< DDD / Domain Event
 @Data
-@ToString
 public class DeliveryStarted extends AbstractEvent {
 
     private Long id;
@@ -16,13 +13,4 @@ public class DeliveryStarted extends AbstractEvent {
     private String customerId;
     private Integer quantity;
     private Long orderId;
-
-    public DeliveryStarted(Delivery aggregate) {
-        super(aggregate);
-    }
-
-    public DeliveryStarted() {
-        super();
-    }
 }
-//>>> DDD / Domain Event
---
+++ customer-center/src/main/java/labshopcompensation/domain/MyPage.java
@@ -0,0 +1,19 @@
+package labshopcompensation.domain;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+
+//<<< EDA / CQRS
+@Entity
+@Table(name = "MyPage_table")
+@Data
+public class MyPage {
+
+    private Long orderId;
+    private String productId;
+    private String orderStatus;
+    private String deliveryStatus;
+}
